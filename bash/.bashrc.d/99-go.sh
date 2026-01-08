c2fd2db39afe006684c4a932d5a06284fef36e29
pengchang <changpeng94@gmail.com>
2026-01-08 11:48:25 +0800

    change golang env

--- bash/.bashrc.d/99-go.sh
+++ bash/.bashrc.d/99-go.sh
@@ -1,3 +1,7 @@
-    export GO111MODULE=on
-    export GOPROXY=https://goproxy.cn
+# /usr/local/go/bin
+if [[ -d "/path/to/dir" ]]; then
     export PATH=$PATH:~/go/bin:/usr/local/go/bin
+fi
+export go111module=on
+export GOPROXY=https://goproxy.cn
+
